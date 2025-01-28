d7a665c0b079fcef483310d6cea04527f1057f3b
Cortesja <146901338+Cortesja@users.noreply.github.com>
2025-01-28 15:13:25 +0900

    Attackの生成する方向が治りました。

--- sotsugyouseisaku/Assets/Scripts/AttackManager.cs
+++ sotsugyouseisaku/Assets/Scripts/AttackManager.cs
@@ -14,10 +14,8 @@ public class AttackManager : MonoBehaviour
 
     private const string FLOOR_LAYER_NAME = "Floor";
 
-    private Vector3 destination_;
     private Vector3 lookAtPoint_;
     private float speed_ = 30.0f;
-    [SerializeField] private Transform player_;
 
     [SerializeField, Header("Prefabs")]
     private FireBall fireballPrefab_;
@@ -45,48 +43,13 @@ public class AttackManager : MonoBehaviour
 
     void ShootProjectile()
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit raycastHit;
-        //bool isHit = Physics.Raycast(
-        //    ray,
-        //    out raycastHit,
-        //    1000,
-        //    LayerMask.GetMask(FLOOR_LAYER_NAME));
-
-        //if (isHit)
-        //{
-        //    destination_ = raycastHit.point;
-        //}
-
-        //RaycastHit hit;
-
-        //if (Physics.Raycast(ray, out hit))
-        //{
-        //    destination_ = hit.point;
-        //}
-        //else
-        //{
-        //    destination_ = ray.GetPoint(1000);
-        //}
-
-        Ray ray = cursor_.GetRay();
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            destination_ = hit.point;
-        }
-        else
-        {
-            destination_ = ray.GetPoint(1000);
-        }
-
         ////////////////////////////////////////////////////////
 
         Vector3 cursorPoint = cursor_.GetRaycastHit().point;
         Vector3 toCamera = -cursor_.GetRay().direction;
         float dot = Vector3.Dot(Vector3.up, toCamera);
         float angleRad = Mathf.Acos(dot);
-        float h = player_.position.y - cursorPoint.y;
+        float h = transform.position.y - cursorPoint.y;
         float cLength = h / Mathf.Cos(angleRad);
         lookAtPoint_ = cursorPoint + toCamera * cLength;
 
@@ -95,17 +58,15 @@ public class AttackManager : MonoBehaviour
         ///////////////////////////////////////////////////////////
 
         //debug
-        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red, 2.0f);
+        Debug.DrawRay(cursor_.GetRay().origin, cursor_.GetRay().direction * 1000, Color.red, 2.0f);
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.green, 2.0f);
 
         InstantiateProjectile();
     }
 
     void InstantiateProjectile()
     {
-        //adjust y value so that can see
-        Vector3 offset = player_.position;
-        offset.y += 1.0f;
-        var projectileObj = Instantiate(projectile_, offset, Quaternion.identity) as GameObject;
-        projectileObj.GetComponent<Rigidbody>().velocity = (destination_ - player_.position).normalized * speed_;
+        var projectileObj = Instantiate(projectile_, transform.position, Quaternion.identity) as GameObject;
+       projectileObj.GetComponent<Rigidbody>().velocity = transform.forward * speed_;
     }
 }
